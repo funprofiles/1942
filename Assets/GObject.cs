73d0d47ba35c8d3a12d13fef016b5f052ef73895
funprofiles <linus.ericsson.98@gmail.com>
2016-02-12 10:21:06 +0100

    game
    ...

---
+++ Assets/GObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class GObject : MonoBehaviour {
+
+    protected Vector3 direction;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public virtual void Update () {
+        transform.position += direction * speed * Time.deltaTime;
+	
+	}
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerMovement : GObject {
+    Rigidbody2D rb;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    public override void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            direction.x = -1;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            direction.x=1;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            direction.y=-1;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            direction.y=1;
+        }
+
+        base.Update();
+    }
+
+   
+}
